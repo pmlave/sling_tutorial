95cd811cac974a87ad08fb3560d82c4cf93d0eee
Ryan Keenan <ryan@infielddigital.com>
2019-06-06 11:49:28 -0600

    Removing extra html files now that they are included in ui

--- html-files/content-creator.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(() => {
-  var keyValueNum = 0;
-  $('#new-key-value').on('click', () => {
-    var thing = $('#key-value-pair').html();
-    thing = thing.replace(new RegExp("{{number}}", "g"), keyValueNum);
-    $('#key-value-dest').append(thing);
-    keyValueNum++;
-  });
-});
